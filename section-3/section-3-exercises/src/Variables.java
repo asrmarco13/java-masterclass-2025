849dc5f43ca4f35725682bf9541196cafee9044c
Marco Orfei <marco.orfei@iad2.it>
2024-12-17 17:55:30 +0100

    Update class variables

--- section-3/section-3-exercises/src/Variables.java
+++ section-3/section-3-exercises/src/Variables.java
@@ -1,6 +1,6 @@
 public class Variables {
     public static void main(String[] args) {
         int myFirstNumber = 5;
-        System.out.println(myFirstNumber);
+        System.out.println("My first number is " + myFirstNumber);
     }
 }
